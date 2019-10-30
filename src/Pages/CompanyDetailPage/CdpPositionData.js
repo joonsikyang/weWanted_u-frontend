09a356371a56177d0367dad0246cc54aed07720e
김관희 <rhks122314@gmail.com>
2019-10-30 17:51:05 +0900

    company detail  page text input completed

--- src/Pages/CompanyDetailPage/CdpPositionData.js
+++ src/Pages/CompanyDetailPage/CdpPositionData.js
@@ -1,6 +1,6 @@
 export const CdpCompanyInfoData = [
   {
-    id: "avs3df",
+    id: 1,
     companyName: "브레이브모바일(숨고, Soomgo)",
     logoImg: "",
     detailImg1:
@@ -108,176 +108,262 @@ export const CdpCompanyInfoData = [
     country: "한국"
   }
 ];
-export const CdpPositionData = [
-  {
-    id: 1,
-    companyName: "브레이브모바일(숨고, Soomgo)",
-    positionName: "Front-End웹 개발자",
-    deadLine: "2019.11.01",
-    followCount: 14,
-    자격요건: `- 프론트엔드 개발 경력 5년 이상
-   - React 프레임워크 사용 경험
-   - Webpack과 같은 모듈 번들러 사용 경험이 있으신 분
-   - Git & Github, HTTP, RESTful API 클라이언트 개발 경험
-   - HTML5 / CSS3 / Javascript 웹 프론트 개발 능력
-   - 웹프론트 개발에 대한 포트폴리오 제출 필수
-   - RESTful API를 이용한 백엔드 연동 개발 경험
-   - 스스로 업무/시간 관리가 가능한 분
-   - 커뮤니케이션이 유연하고 Co-work에 익숙한 분
-   * 이력서에 현재/희망연봉 기재 필수`,
-    우대사항: `- 최신 프론트엔드 기술에 대한 이해와 빠른 습득 능력
-   - Javascript ES6버전 이상 개발 경험 우대
-   - 서비스 런칭 경험
-   - 의료정보 서비스 개발 경험
-   - 라이브 웹서비스 혹은 웹앱 개발 경험
-   - 디자이너, 백엔드 엔지니어와의 원활한 커뮤니케이션 능력`,
-    주요업무: `- 신규서비스 웹 어플리케이션 프론트엔드 개발
-   - RESTful API 통신 개발`
-  },
-  {
-    id: "b",
-    companyName: "브레이브모바일(숨고, Soomgo)",
-    positionName: "프론트엔드 웹 개발자(신입 가능)",
-    deadLine: "2019.12.24",
-    followCount: 10,
-    자격요건: `• HTML5, CSS3 등 웹 표준에 대한 이해도가 높은 분
-    • Javascript에 능숙하고, React 관련 개발 경험이 있으신 분
-    • 크로스브라우징에 대한 기본 이해를 갖추신 분
-    • 버전관리 시스템(git, svn)을 사용한 경험이 있으신 분
-    • Back-end 개발자 및 디자이너와 원활한 커뮤니케이션이 가능하신 분`,
-    우대사항: `• 전산/컴퓨터 공학 관련 전공자
-    • PHP 기반 프로젝트 경험이 있으신 분
-    • Redux, MobX를 이용한 상태 관리 경험 및 이해도가 있으신 분
-    • Jira 활용 및 Confluence 문서화에 익숙하신 분
-    • 최신 에듀테크 트렌드에 관심이 많은 분`,
-    주요업무: `• 하버드, 예일 등 북미 아이비리그 대학 대다수에서 사용하는 Canvas를 기반으로 국내 교육 환경에 맞게 최적화한 러닝 플랫폼 LMS(Learning Management System)와 국내 100여개 이상의 대학 및 관공서에서 사용하는 CMS(Content Management System) 내에 주요 화면과 UI 컴포넌트를 개발
-    • 그 외 미래형 학습 솔루션(Blended learning, Flipped learning, MOOC 등) 관련 신규 제품의 주요 화면과 UI 컴포넌트 개발`
-  },
-  {
-    id: "c",
-    companyName: "브레이브모바일(숨고, Soomgo)",
-    positionName: "프론트엔드 개발자",
-    deadLine: "2019.12.10",
-    followCount: 14,
-    자격요건: `• Javascript, ES6 및 TypeScript 에 대한 깊은 이해
-    • Angular, React 등의 웹 프론트엔드 프레임워크 사용 경험
-    • Webpack, Yarn 등의 툴 사용 경험
-    • HTML5, CSS, SASS 기술에 대한 이해
-    • Git 사용 경험`,
-    우대사항: `• REST API에 대한 이해와 설계 경험
-    • AWS 클라우드 사용 경험
-    • Node.js를 이용한 개발 경험
-    • SaaS 서비스에 대한 이해와 상용 경험`,
-    주요업무: `• 깃플 서비스 프론트엔드 UI/UX 개발
-    • 문의고객용 채팅앱 및 상담/관리용 워크스페이스(https://workspace.gitple.io) 개발
-    • Angularjs + Typescript`
-  },
-  {
-    id: "d",
-    companyName: "브레이브모바일(숨고, Soomgo)",
-    positionName: "프론트엔드 개발자",
-    deadLine: "2019.12.10",
-    followCount: 15,
-    자격요건: `•모집대상 공통
-    •웹 프론트엔드 개발이 가능한 사람 (html, javascript, css)
-    •Angular, Vue, React 등의 Web Framework 를 사용해본 경험이 있는 사람`,
-    우대사항: `•Full-Stack Web Developer
-    •Angular 사용에 능숙한 사람
-    •JavaScript 개발 환경 및 웹 소프트웨어 생애 주기에 대한 깊은 이해가 있는사람
-    •CI / CD 경험자
-    •오픈소스 프로젝트 기여 경험`,
-    주요업무: `주요 업무 : 프론트엔드 응용 프로그램 및 프레임워크 개발
-    (지원자의 선호/역량에 따라 실제 업무는 아래와 다소 달라질 수 있습니다)
-    •사용자가 로봇 서비스를 쉽게 설치하고, 운영 및 이용할 수 있는 Web UI 개발
-    •멀티 로봇 서비스를 위한 환경 설정/모니터링/운영 결과 보고를 위한 UI 개발
-    •로봇 서비스를 활용하기 위한 사용자 웹어플리케이션 개발
-    •개별 로봇 운영을 위한 웹 어플리케이션 개발`
-  },
-  {
-    id: 1,
-    companyName: "브레이브모바일(숨고, Soomgo)",
-    positionName: "Front-End웹 개발자",
-    deadLine: "2019.11.01",
-    followCount: 14,
-    자격요건: `- 프론트엔드 개발 경력 5년 이상
-   - React 프레임워크 사용 경험
-   - Webpack과 같은 모듈 번들러 사용 경험이 있으신 분
-   - Git & Github, HTTP, RESTful API 클라이언트 개발 경험
-   - HTML5 / CSS3 / Javascript 웹 프론트 개발 능력
-   - 웹프론트 개발에 대한 포트폴리오 제출 필수
-   - RESTful API를 이용한 백엔드 연동 개발 경험
-   - 스스로 업무/시간 관리가 가능한 분
-   - 커뮤니케이션이 유연하고 Co-work에 익숙한 분
-   * 이력서에 현재/희망연봉 기재 필수`,
-    우대사항: `- 최신 프론트엔드 기술에 대한 이해와 빠른 습득 능력
-   - Javascript ES6버전 이상 개발 경험 우대
-   - 서비스 런칭 경험
-   - 의료정보 서비스 개발 경험
-   - 라이브 웹서비스 혹은 웹앱 개발 경험
-   - 디자이너, 백엔드 엔지니어와의 원활한 커뮤니케이션 능력`,
-    주요업무: `- 신규서비스 웹 어플리케이션 프론트엔드 개발
-   - RESTful API 통신 개발`
-  },
-  {
-    id: "b",
-    companyName: "브레이브모바일(숨고, Soomgo)",
-    positionName: "프론트엔드 웹 개발자(신입 가능)",
-    deadLine: "2019.12.24",
-    followCount: 10,
-    자격요건: `• HTML5, CSS3 등 웹 표준에 대한 이해도가 높은 분
-    • Javascript에 능숙하고, React 관련 개발 경험이 있으신 분
-    • 크로스브라우징에 대한 기본 이해를 갖추신 분
-    • 버전관리 시스템(git, svn)을 사용한 경험이 있으신 분
-    • Back-end 개발자 및 디자이너와 원활한 커뮤니케이션이 가능하신 분`,
-    우대사항: `• 전산/컴퓨터 공학 관련 전공자
-    • PHP 기반 프로젝트 경험이 있으신 분
-    • Redux, MobX를 이용한 상태 관리 경험 및 이해도가 있으신 분
-    • Jira 활용 및 Confluence 문서화에 익숙하신 분
-    • 최신 에듀테크 트렌드에 관심이 많은 분`,
-    주요업무: `• 하버드, 예일 등 북미 아이비리그 대학 대다수에서 사용하는 Canvas를 기반으로 국내 교육 환경에 맞게 최적화한 러닝 플랫폼 LMS(Learning Management System)와 국내 100여개 이상의 대학 및 관공서에서 사용하는 CMS(Content Management System) 내에 주요 화면과 UI 컴포넌트를 개발
-    • 그 외 미래형 학습 솔루션(Blended learning, Flipped learning, MOOC 등) 관련 신규 제품의 주요 화면과 UI 컴포넌트 개발`
-  },
-  {
-    id: "c",
-    companyName: "브레이브모바일(숨고, Soomgo)",
-    positionName: "프론트엔드 개발자",
-    deadLine: "2019.12.10",
-    followCount: 14,
-    자격요건: `• Javascript, ES6 및 TypeScript 에 대한 깊은 이해
-    • Angular, React 등의 웹 프론트엔드 프레임워크 사용 경험
-    • Webpack, Yarn 등의 툴 사용 경험
-    • HTML5, CSS, SASS 기술에 대한 이해
-    • Git 사용 경험`,
-    우대사항: `• REST API에 대한 이해와 설계 경험
-    • AWS 클라우드 사용 경험
-    • Node.js를 이용한 개발 경험
-    • SaaS 서비스에 대한 이해와 상용 경험`,
-    주요업무: `• 깃플 서비스 프론트엔드 UI/UX 개발
-    • 문의고객용 채팅앱 및 상담/관리용 워크스페이스(https://workspace.gitple.io) 개발
-    • Angularjs + Typescript`
-  },
-  {
-    id: "d",
-    companyName: "브레이브모바일(숨고, Soomgo)",
-    positionName: "프론트엔드 개발자",
-    deadLine: "2019.12.10",
-    followCount: 15,
-    자격요건: `•모집대상 공통
-    •웹 프론트엔드 개발이 가능한 사람 (html, javascript, css)
-    •Angular, Vue, React 등의 Web Framework 를 사용해본 경험이 있는 사람`,
-    우대사항: `•Full-Stack Web Developer
-    •Angular 사용에 능숙한 사람
-    •JavaScript 개발 환경 및 웹 소프트웨어 생애 주기에 대한 깊은 이해가 있는사람
-    •CI / CD 경험자
-    •오픈소스 프로젝트 기여 경험`,
-    주요업무: `주요 업무 : 프론트엔드 응용 프로그램 및 프레임워크 개발
-    (지원자의 선호/역량에 따라 실제 업무는 아래와 다소 달라질 수 있습니다)
-    •사용자가 로봇 서비스를 쉽게 설치하고, 운영 및 이용할 수 있는 Web UI 개발
-    •멀티 로봇 서비스를 위한 환경 설정/모니터링/운영 결과 보고를 위한 UI 개발
-    •로봇 서비스를 활용하기 위한 사용자 웹어플리케이션 개발
-    •개별 로봇 운영을 위한 웹 어플리케이션 개발`
-  }
-];
+export const CdpPositionData = {
+  id: 1,
+  companyName: "브레이브모바일(숨고, Soomgo)",
+  positionName: "Front-End웹 개발자",
+  deadLine: "2019.11.01",
+  followCount: 14,
+  /* eslint-disable */
+  intro: 
+  `
+[Beginning of Soomgo Journey]
+"숨고는 사람과 사람 사이의 진정성 있는 연결로 우리의 라이프&워크를 향상시킵니다."
+
+  내가 원하는 조건에 딱 맞는 전문가를 찾는 것은 여전히 어려운 일입니다. 
+  인테리어, 청소부터 취미 레슨, 과외, 그리고 비즈니스 서비스까지.
+  이곳저곳 발품팔지 않고, 숨고에서, 오늘 바로,
+  더 편리한 일상, 더 풍성한 라이프를 만들어줄 고수를 찾을 수 있습니다.
+  
+  소상공인, 프리랜서로서 신규 고객을 찾는 것은 여전히 어려운 일입니다.
+  다양한 분야의 고수님들이 더 쉽고 빠르게 고객을 만날 수 있도록 합니다.
+  이곳저곳 발품팔지 않고,  숨고에서, 오늘 바로,
+  나의 브랜드, 나의 비즈니스 구축을 시작할 수 있습니다.
+  숨고는 사람과 사람, 비즈니스와 비즈니스를 효율적으로 연결하고,
+  라이프-워크에 유의미한 변화의 시작을 일으킵니다.
+
+[SBA 서울산업진흥원에서 추천하는 숨고 클릭!]
+• 슈퍼기업관: http://www.jobkorea.co.kr/Super/soomgo
+• 더빅 기업인터뷰: https://bit.ly/2FkuWaE
+
+[숨고매거진 -숨고 멤버로 살아가기]
+• https://brunch.co.kr/magazine/howsoomgoworks
+
+[숨고에서 나와 함께 일할 멤버가 궁금하나요?]
+• Tech (Back-end) Greg: www.morenice.kr
+• Tech (Mobile) Floyd: https://floydkim.netlify.com 
+• Marketing Jay : https://bit.ly/2W0z0S8
+• Tech Alan& Sales Paul : https://bit.ly/2H1MieJ
+• Product Reo : https://bit.ly/2PiK5P0
+• Data Analyst Ohs: https://bit.ly/2D5kaFt
+
+[관련 기사]
+1. 숨고, 시리즈B 125억 투자 유치!
+https://www.venturesquare.net/783616
+2. 자율출퇴근,재택근무 등 알짜 슈퍼기업 어디?
+https://www.instagram.com/p/ByUurljDs1S/?igshid=f36st3whbudi
+3. (주)브레이브모바일, SBA 주최 2018 우수 채용기업 3위 선정
+http://news.hankyung.com/article/201812280132a
+
+[숨고 Tech팀 철학] 
+1. 진짜 애자일(Agile)한 협업을 추구합니다.
+ 팀원간의 신뢰를 통한 건강한 개발 프로세스를 지향합니다. 개발, 제품, 디자인까지 팀웍을 기반으로 유연하게 구성되어 있습니다. 기획 단계부터 부터 개발, 배포 완료까지 기획, 디자이너, 개발자 모두 참여합니다. 또한 매 스프린트마다 만들 것을 서로 플래닝하면서 정말 중요한 일이 무엇인지 파악하고, 그외의 것은 과감히 내려 놓습니다. 또한 의사소통이 어렵거나 추후 유지보수를 위해서 모든 프로세스는 JIRA, Confluence 등의 업무툴을 이용한 실용적인 문서화를 지향하고 있습니다.
+  
+2. 우리가 가장 잘할 수 있는 일을 합니다.
+ 우리는 오로지 숨고를 위한 서비스에 집중하며, 우리 고객의 만족을 위해 개발을 합니다. 우리가 가장 잘 할 수 있는 기술 개발 외에는 현재 AWS(서버인프라), ElasticSearch(검색), LogDNA(로그), NewRelic(모니터링), Amplitude(유저행동분석), Segment(통계취합), Braze(고객CRM)등 여러 기술솔루션을 적극 도입하고 집중력을 유지합니다.
+
+3. 숨고 개발의 지속적인 성장을 추구합니다. 
+ 서로의 코드를 리뷰 해가면서 고민을 공유하고, 구현한 코드를 분석하면서 서로 간의 기술을 배울 수  있습니다. 또한 서로 다른 기술 분야의 이해와 플랫폼 전체의 발전을 위해서 내부 기술세미나를 진행합니다. 이를 통해, 현재까지 만들어진 숨고 전체 기술 스택은 다음과 같습니다. (https://stackshare.io/soomgo/soomgo)`,
+
+  benefits: `
+(지속적으로 업그레이드 중입니다)
+근무시간
+• 오전 9-11시 사이 자율출근 / 주 40시간 근무제
+• 눈치보지 않고 쓰는 연차 
+• 월 1회 재택근무
+
+근무환경
+• 루나랩 원터치 스탠딩 책상 전원 제공
+• 최적의 업무 장비 지원
+• 신규 입사자 welcome kit 제공
+
+성장지원
+• 매년 더 많은 교육비, 도서비 지원
+• 숨고 서비스 이용 체험비 매년 추가 지원
+• 활발한 사내 스터디와 세미나 
+
+가족지원
+• 명절 상품권 지급
+• 생일날 케익, 상품권 + 조기퇴근
+• 탄탄한 경조휴가와 경조금
+
+기타지원
+• 매월 멤버들과 점심 맛집탐방 지원
+• 회식은 먹고 싶은거 맘편히 먹는 날
+• 원하는 간식 무한제공
+• 청년내일채움공제, 청소년소득세감면 적극 직원
+• 건강검진 지원
+• 야근 택시비, 야근 식대 제공 
+
+[숨고멤버 Join]
+• 서류 : 이력서(자기소개서) 및 경력 기술서
+• 코드 테스트 : (서류전형 합격시) 코드테스트 전달 예정. 
+• 인터뷰 : 코드테스트 피드백 및 숨고 Fit, R&R 논의 
+• Finally, 숨고 멤버로 입사!`,
+  main_tasks: `
+[숨고 프론트엔드 개발자]
+숨고는 놀랍게 발전하는 자바스크립트를 사용하여 제대로 된 프론트엔드를 개발하고 싶습니다. 우리는 현재 Vue.js을 베이스로 개발하고 있으며, 점진적으로 모든 통신을 Restful API로 백엔드와 프론트엔드를 분리하고 테스트 자동화까지 구현 가능한 환경을 만들고 있습니다.
+
+• 숨고 서비스의 웹 클라이언트 개발
+• 노드 기반의 프론트엔드 렌더 서버 운영
+• SSR(Server Sider Rendering) 지원
+• Typescript, 테스트 코드 커버리지 관리`,
+  preferred_points: `
+• 웹 표준, 반응형 디자인을 고려한 마크업 UI개발 경험
+• 엔지니어링 관련해서 문서를 작성하고 공유할 수 있는 분
+• 프론트엔드 Framework (Vue, React, Angular) 경험이 있는 분
+• Typescript를 사용해봤거나 관련 지식이 있는 분
+• 웹사이트 성능 측정, 분석, 개선에 관심 있는 분`,
+  requirements: `
+• 5년 이상 경력 수준의 HTML, CSS등 W3C 웹 표준에 대한 이해
+• JavaScript ES6와 같은 Modern JavaScript에 대한 활용 능력
+• 성능 최적화에 대해 지속적으로 고민하고 노력하시는 분`
+};
+ /* eslint-enable */
+//   {
+//     id: "b",
+//     companyName: "브레이브모바일(숨고, Soomgo)",
+//     positionName: "프론트엔드 웹 개발자(신입 가능)",
+//     deadLine: "2019.12.24",
+//     followCount: 10,
+//     자격요건: `• HTML5, CSS3 등 웹 표준에 대한 이해도가 높은 분
+//     • Javascript에 능숙하고, React 관련 개발 경험이 있으신 분
+//     • 크로스브라우징에 대한 기본 이해를 갖추신 분
+//     • 버전관리 시스템(git, svn)을 사용한 경험이 있으신 분
+//     • Back-end 개발자 및 디자이너와 원활한 커뮤니케이션이 가능하신 분`,
+//     우대사항: `• 전산/컴퓨터 공학 관련 전공자
+//     • PHP 기반 프로젝트 경험이 있으신 분
+//     • Redux, MobX를 이용한 상태 관리 경험 및 이해도가 있으신 분
+//     • Jira 활용 및 Confluence 문서화에 익숙하신 분
+//     • 최신 에듀테크 트렌드에 관심이 많은 분`,
+//     주요업무: `• 하버드, 예일 등 북미 아이비리그 대학 대다수에서 사용하는 Canvas를 기반으로 국내 교육 환경에 맞게 최적화한 러닝 플랫폼 LMS(Learning Management System)와 국내 100여개 이상의 대학 및 관공서에서 사용하는 CMS(Content Management System) 내에 주요 화면과 UI 컴포넌트를 개발
+//     • 그 외 미래형 학습 솔루션(Blended learning, Flipped learning, MOOC 등) 관련 신규 제품의 주요 화면과 UI 컴포넌트 개발`
+//   },
+//   {
+//     id: "c",
+//     companyName: "브레이브모바일(숨고, Soomgo)",
+//     positionName: "프론트엔드 개발자",
+//     deadLine: "2019.12.10",
+//     followCount: 14,
+//     자격요건: `• Javascript, ES6 및 TypeScript 에 대한 깊은 이해
+//     • Angular, React 등의 웹 프론트엔드 프레임워크 사용 경험
+//     • Webpack, Yarn 등의 툴 사용 경험
+//     • HTML5, CSS, SASS 기술에 대한 이해
+//     • Git 사용 경험`,
+//     우대사항: `• REST API에 대한 이해와 설계 경험
+//     • AWS 클라우드 사용 경험
+//     • Node.js를 이용한 개발 경험
+//     • SaaS 서비스에 대한 이해와 상용 경험`,
+//     주요업무: `• 깃플 서비스 프론트엔드 UI/UX 개발
+//     • 문의고객용 채팅앱 및 상담/관리용 워크스페이스(https://workspace.gitple.io) 개발
+//     • Angularjs + Typescript`
+//   },
+//   {
+//     id: "d",
+//     companyName: "브레이브모바일(숨고, Soomgo)",
+//     positionName: "프론트엔드 개발자",
+//     deadLine: "2019.12.10",
+//     followCount: 15,
+//     자격요건: `•모집대상 공통
+//     •웹 프론트엔드 개발이 가능한 사람 (html, javascript, css)
+//     •Angular, Vue, React 등의 Web Framework 를 사용해본 경험이 있는 사람`,
+//     우대사항: `•Full-Stack Web Developer
+//     •Angular 사용에 능숙한 사람
+//     •JavaScript 개발 환경 및 웹 소프트웨어 생애 주기에 대한 깊은 이해가 있는사람
+//     •CI / CD 경험자
+//     •오픈소스 프로젝트 기여 경험`,
+//     주요업무: `주요 업무 : 프론트엔드 응용 프로그램 및 프레임워크 개발
+//     (지원자의 선호/역량에 따라 실제 업무는 아래와 다소 달라질 수 있습니다)
+//     •사용자가 로봇 서비스를 쉽게 설치하고, 운영 및 이용할 수 있는 Web UI 개발
+//     •멀티 로봇 서비스를 위한 환경 설정/모니터링/운영 결과 보고를 위한 UI 개발
+//     •로봇 서비스를 활용하기 위한 사용자 웹어플리케이션 개발
+//     •개별 로봇 운영을 위한 웹 어플리케이션 개발`
+//   },
+//   {
+//     id: 1,
+//     companyName: "브레이브모바일(숨고, Soomgo)",
+//     positionName: "Front-End웹 개발자",
+//     deadLine: "2019.11.01",
+//     followCount: 14,
+//     자격요건: `- 프론트엔드 개발 경력 5년 이상
+//    - React 프레임워크 사용 경험
+//    - Webpack과 같은 모듈 번들러 사용 경험이 있으신 분
+//    - Git & Github, HTTP, RESTful API 클라이언트 개발 경험
+//    - HTML5 / CSS3 / Javascript 웹 프론트 개발 능력
+//    - 웹프론트 개발에 대한 포트폴리오 제출 필수
+//    - RESTful API를 이용한 백엔드 연동 개발 경험
+//    - 스스로 업무/시간 관리가 가능한 분
+//    - 커뮤니케이션이 유연하고 Co-work에 익숙한 분
+//    * 이력서에 현재/희망연봉 기재 필수`,
+//     우대사항: `- 최신 프론트엔드 기술에 대한 이해와 빠른 습득 능력
+//    - Javascript ES6버전 이상 개발 경험 우대
+//    - 서비스 런칭 경험
+//    - 의료정보 서비스 개발 경험
+//    - 라이브 웹서비스 혹은 웹앱 개발 경험
+//    - 디자이너, 백엔드 엔지니어와의 원활한 커뮤니케이션 능력`,
+//     주요업무: `- 신규서비스 웹 어플리케이션 프론트엔드 개발
+//    - RESTful API 통신 개발`
+//   },
+//   {
+//     id: "b",
+//     companyName: "브레이브모바일(숨고, Soomgo)",
+//     positionName: "프론트엔드 웹 개발자(신입 가능)",
+//     deadLine: "2019.12.24",
+//     followCount: 10,
+//     자격요건: `• HTML5, CSS3 등 웹 표준에 대한 이해도가 높은 분
+//     • Javascript에 능숙하고, React 관련 개발 경험이 있으신 분
+//     • 크로스브라우징에 대한 기본 이해를 갖추신 분
+//     • 버전관리 시스템(git, svn)을 사용한 경험이 있으신 분
+//     • Back-end 개발자 및 디자이너와 원활한 커뮤니케이션이 가능하신 분`,
+//     우대사항: `• 전산/컴퓨터 공학 관련 전공자
+//     • PHP 기반 프로젝트 경험이 있으신 분
+//     • Redux, MobX를 이용한 상태 관리 경험 및 이해도가 있으신 분
+//     • Jira 활용 및 Confluence 문서화에 익숙하신 분
+//     • 최신 에듀테크 트렌드에 관심이 많은 분`,
+//     주요업무: `• 하버드, 예일 등 북미 아이비리그 대학 대다수에서 사용하는 Canvas를 기반으로 국내 교육 환경에 맞게 최적화한 러닝 플랫폼 LMS(Learning Management System)와 국내 100여개 이상의 대학 및 관공서에서 사용하는 CMS(Content Management System) 내에 주요 화면과 UI 컴포넌트를 개발
+//     • 그 외 미래형 학습 솔루션(Blended learning, Flipped learning, MOOC 등) 관련 신규 제품의 주요 화면과 UI 컴포넌트 개발`
+//   },
+//   {
+//     id: "c",
+//     companyName: "브레이브모바일(숨고, Soomgo)",
+//     positionName: "프론트엔드 개발자",
+//     deadLine: "2019.12.10",
+//     followCount: 14,
+//     자격요건: `• Javascript, ES6 및 TypeScript 에 대한 깊은 이해
+//     • Angular, React 등의 웹 프론트엔드 프레임워크 사용 경험
+//     • Webpack, Yarn 등의 툴 사용 경험
+//     • HTML5, CSS, SASS 기술에 대한 이해
+//     • Git 사용 경험`,
+//     우대사항: `• REST API에 대한 이해와 설계 경험
+//     • AWS 클라우드 사용 경험
+//     • Node.js를 이용한 개발 경험
+//     • SaaS 서비스에 대한 이해와 상용 경험`,
+//     주요업무: `• 깃플 서비스 프론트엔드 UI/UX 개발
+//     • 문의고객용 채팅앱 및 상담/관리용 워크스페이스(https://workspace.gitple.io) 개발
+//     • Angularjs + Typescript`
+//   },
+//   {
+//     id: "d",
+//     companyName: "브레이브모바일(숨고, Soomgo)",
+//     positionName: "프론트엔드 개발자",
+//     deadLine: "2019.12.10",
+//     followCount: 15,
+//     자격요건: `•모집대상 공통
+//     •웹 프론트엔드 개발이 가능한 사람 (html, javascript, css)
+//     •Angular, Vue, React 등의 Web Framework 를 사용해본 경험이 있는 사람`,
+//     우대사항: `•Full-Stack Web Developer
+//     •Angular 사용에 능숙한 사람
+//     •JavaScript 개발 환경 및 웹 소프트웨어 생애 주기에 대한 깊은 이해가 있는사람
+//     •CI / CD 경험자
+//     •오픈소스 프로젝트 기여 경험`,
+//     주요업무: `주요 업무 : 프론트엔드 응용 프로그램 및 프레임워크 개발
+//     (지원자의 선호/역량에 따라 실제 업무는 아래와 다소 달라질 수 있습니다)
+//     •사용자가 로봇 서비스를 쉽게 설치하고, 운영 및 이용할 수 있는 Web UI 개발
+//     •멀티 로봇 서비스를 위한 환경 설정/모니터링/운영 결과 보고를 위한 UI 개발
+//     •로봇 서비스를 활용하기 위한 사용자 웹어플리케이션 개발
+//     •개별 로봇 운영을 위한 웹 어플리케이션 개발`
+//   }
+// ];
 
 export const Skills_DATA = [
   {
