0a6417e8c79e5dac4de2a2af6e8f9a990ce5ebac
Joon Sik Yang <joonsikyang@gmail.com>
2019-11-01 15:37:47 +0900

    Routes default route changed from MainPage to LoginSignupPage

--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LoginSignUpPage from "Pages/LoginSignUpPage";
 import CompanyListPage from "Pages/CompanyListPage";
-import MainPage from "Pages/MainPage";
 import MakePortfolioPage from "Pages/MakePortfolioPage";
 
 class Routes extends React.Component {
@@ -10,9 +9,8 @@ class Routes extends React.Component {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={MainPage} />
+          <Route exact path="/" component={LoginSignUpPage} />
           <Route exact path="/company_list" component={CompanyListPage} />
-          <Route exact path="/login_signup" component={LoginSignUpPage} />
           <Route exact path="/make_portfolio" component={MakePortfolioPage} />
         </Switch>
       </Router>
